53f3006a9aca7613d05563a5da7e332bc0842ccc
jakejack13 <jckawesome13@gmail.com>
2018-03-07 21:02:05 -0500

    Fixed save file system and began on new game state

--- functions.py
+++ functions.py
@@ -1,4 +1,11 @@
 def new_save(name) :
     s_file = open("save.txt","w")
-    s_file.write(name)
+    #Name, Lvl, Atk, Def
+    s_file.write(name + "#0" + "#0" + "#0")
     s_file.close()
+def load_save() :
+    s_file = open("save.txt","r")
+    save_data = s_file.read()
+    stats = save_data.split("#")
+    s_file.close()
+    return stats
--- master_script.py
+++ master_script.py
@@ -1,2 +1,15 @@
+import os.path
+from pathlib import Path
 from functions import new_save
-new_save(input("Please input your name: "))
+from functions import load_save
+
+print("Journey Down")
+print("By Jacob Kerr, 2018")
+#Creating and loading save file and stored as stats
+my_file = Path("save.txt")
+if not my_file.is_file():
+    new_save(input("Please enter your name: "))
+    new_game = 1
+else:
+    new_game = 0
+stats = load_save()
